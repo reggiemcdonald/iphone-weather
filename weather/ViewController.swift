850d06b118f0c5e48ba35d0dbbdf377564bf8de8
reggiemcdonald <regmcdonald95@gmail.com>
2019-06-24 12:56:42 -0700

    implemented a weather message parser

--- weather/ViewController.swift
+++ weather/ViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController, Observer {
+    var id: Int = 1;
+    
+    @IBOutlet weak var backgroundImg: UIImageView!
+    var weatherManager: WeatherManager!;
+    @IBOutlet weak var weatherMessage: UITextField!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        super.viewDidLoad();
+        weatherManager = WeatherManager();
+        weatherManager.attachObserver(observer: self);
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // DO stuff
+        
+    }
+    
+    public func update(weather: String) {
+        weatherMessage.text = weather;
+    }
+    
+    func update() {
+        weatherMessage.text = weatherManager.getWeather() ?? "Invalid City";
+    }
+    
+
 
 
 }
