0a98ade56e1e8c9c6bca5423407c610d3ea0eb82
reggiemcdonald <regmcdonald95@gmail.com>
2019-06-24 10:16:01 -0700

    initial commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Reginald McDonald on 2019-06-24.
+//  Copyright © 2019 Reginald McDonald. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
